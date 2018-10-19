5433d5f66a8b4234f80e58d18d36537c72dbdca2
Sorokin-Oleg <sorokin.world@gmail.com>
2018-10-19 10:53:51 +0300

    - Added the function of opening and closing locks - Fixed some bugs

--- static/js/gpio/main.js
+++ static/js/gpio/main.js
@@ -1,34 +1,32 @@
+"use strict";
+
 $(function(){
-// Variables
 
 // Create button on/off and out/in
     $(".btn-gpio").click(function(){
         $(this).toggleClass("btn-on btn-off");
         let nameButton = $(this).text();
-            switch (nameButton){
-                case "OFF":
-                    $(this).text("ON");
-                    break;
-                case "ON":
-                    $(this).text("OFF");
-                    break;
-                case "OUT":
-                    $(this).text("IN");
-                    break;
-                case "IN":
-                    $(this).text("OUT");
-                    break;
-                default:
-                    $(this).text("not used");
-            }
+        switch (nameButton){
+            case "OFF":
+                $(this).text("ON");
+                break;
+            case "ON":
+                $(this).text("OFF");
+                break;
+            case "OUT":
+                $(this).text("IN");
+                break;
+            case "IN":
+                $(this).text("OUT");
+                break;
+            default:
+                $(this).text("not used");
+        }
     });
 
-    // Create a Tooltip
-    $('[data-toggle="tooltip"]').tooltip();
+    // Function of opening and closing locks
+    $(".lock-icon").click(function () {
+        $(this).toggleClass("fa-lock-open fa-lock");
+    });
 
 });
-
-// Just a class change, without involving jQuery
-function toggleIcon(x) {
-    x.classList.toggle("fa-lock-open")
-}
