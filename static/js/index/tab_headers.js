d03375ecc72e129b35a8c5e514ec78c0a2de5176
Sorokin-Oleg <sorokin.world@gmail.com>
2018-11-10 20:55:42 +0300

    - Created a page for devices - Created a page for users - Designed common styles for the home page

---
+++ static/js/index/tab_headers.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * The written script is used for auxiliary functions of the styles index page
+ * Use the module to isolate variables and functions.
+**/
+(function () {
+
+})();
+
