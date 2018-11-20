be2ea62381740a1cf97f960759ffe5db309d6874
Sorokin-Oleg <sorokin.world@gmail.com>
2018-11-20 14:27:55 +0300

    - Added function of folding aside menu

--- static/js/main/responsive.js
+++ static/js/main/responsive.js
@@ -5,9 +5,34 @@ const BAR = "#toggle-bars";
 /**
 * Auxiliary script for adaptive layout
 **/
+
+$(function() {
+    if (localStorage.getItem("bar") === "true") {
+        // Do display stuff here
+        showBar();
+    } else {
+        // Do hide stuff here
+        hideBar();
+    }
+});
+
 $(document).ready(function () {
     $(BAR).click(function () {
-        $(ASIDE).toggleClass("aside-responsive-off");
-        $(CONTENT).toggleClass("content-responsive-off");
+        if (localStorage.getItem("bar") === "true") {
+            hideBar();
+            localStorage.setItem("bar", "false");
+        } else {
+            showBar();
+            localStorage.setItem("bar", "true");
+        }
     });
 });
+
+function showBar () {
+    $(ASIDE).removeClass("aside-responsive-off").addClass("aside-responsive-on");
+    $(CONTENT).removeClass("content-responsive-off").addClass("content-responsive-on");
+}
+function hideBar () {
+    $(ASIDE).removeClass("aside-responsive-on").addClass("aside-responsive-off");
+    $(CONTENT).removeClass("content-responsive-on").addClass("content-responsive-off");
+}
