de6c114b43713a1f327b228e5ed6e3e63a80e96d
Sorokin-Oleg <sorokin.world@gmail.com>
2018-12-07 21:14:48 +0300

    * Created a modal window template. Without reference to logic.

--- static/js/index/modal_box_index_page.js
+++ static/js/index/modal_box_index_page.js
@@ -5,7 +5,5 @@ const MODAL_DEVICE = "#modal-device";
 $(document).ready(function () {
     $(MODAL_ADD_DEVICE).click(function () {
         $(MODAL_DEVICE).css(DISPLAY_FLEX);
-        // $("body").css({"filter":"blur(2px)"});
-        // $(".modal-content").css({"filter":"blur(0px)"});
     });
 });
