0cb84aae5c93271da189042dc5965f67a43ebb7b
Sorokin-Oleg <sorokin.world@gmail.com>
2018-12-09 19:49:56 +0300

    * Created a new logic for modal window * Fixed bug

--- static/js/index/modal_box_index_page.js
+++ static/js/index/modal_box_index_page.js
@@ -1,14 +1,20 @@
 "use strict";
 const MODAL_ADD_DEVICE = ".modal-add-device";
 const MODAL_EDIT_DEVICE = ".modal-edit-device";
+const MODAL_ADD_USER = ".modal-add-user";
+const MODAL_EDIT_USER = ".modal-edit-user";
 const MODAL_CLOSE_ADD_DEVICE = ".modal-close-add-deviсe";
 const MODAL_CLOSE_EDIT_DEVICE = ".modal-close-edit-deviсe";
+const MODAL_CLOSE_ADD_USER = ".modal-close-add-user";
+const MODAL_CLOSE_EDIT_USER = ".modal-close-edit-user";
 const CONTAINER_DEVICE_ADD = ".container-device-add";
 const CONTAINER_DEVICE_EDIT = ".container-device-edit";
+const CONTAINER_USER_ADD = ".container-user-add";
+const CONTAINER_USER_EDIT = ".container-user-edit";
 const MODAL = ".modal";
-let setTime = 500;
 
 $(document).ready(function () {
+    let setTime = 500;
 
     function openModalAnimation () {
         $(MODAL).removeClass("modal-animation-close");
@@ -43,7 +49,7 @@ $(document).ready(function () {
             $("#device_type").val('1');
         },setTime);
     });
-    // Close modal window (add new device)
+    // Close modal window (edit device)
     $(MODAL_CLOSE_EDIT_DEVICE).click(function () {
         closeModalAnimation();
         setTimeout(function () {
@@ -53,6 +59,34 @@ $(document).ready(function () {
             // $("#device_type").val('1');
         },setTime);
     });
-    // Closes modal window when pressed outside the form.
+
+    /*
+     * Script for modal window new/edit user
+     */
+    // Open modal window (add new user)
+    $(MODAL_ADD_USER).click(function () {
+        openModalAnimation();
+        $(CONTAINER_USER_ADD).css(DISPLAY_FLEX);
+    });
+    // Open modal window (edit user)
+    $(MODAL_EDIT_USER).click(function () {
+       openModalAnimation();
+        $(CONTAINER_USER_EDIT).css(DISPLAY_FLEX);
+    });
+    // Close modal window (add new user)
+    $(MODAL_CLOSE_ADD_USER).click(function () {
+        closeModalAnimation();
+        setTimeout(function () {
+            $(CONTAINER_USER_ADD).css(DISPLAY_NONE);
+        },setTime);
+    });
+    // Close modal window (edit user)
+    $(MODAL_CLOSE_EDIT_USER).click(function () {
+        closeModalAnimation();
+        setTimeout(function () {
+            $(CONTAINER_USER_EDIT).css(DISPLAY_NONE);
+        },setTime);
+    });
+
 
 });
