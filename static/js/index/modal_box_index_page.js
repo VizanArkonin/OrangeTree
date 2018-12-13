3b7d01dd637b298c007da01931a6e66d1155b50e
Sorokin-Oleg <sorokin.world@gmail.com>
2018-12-13 17:18:18 +0300

    * Implementation of the functions of opening / closing modal windows * The second working option. Bug fixes

--- static/js/index/modal_box_index_page.js
+++ static/js/index/modal_box_index_page.js
@@ -1,89 +1,86 @@
 "use strict";
-const MODAL_ADD_DEVICE = ".modal-add-device";
-const MODAL_EDIT_DEVICE = ".modal-edit-device";
-const MODAL_ADD_USER = ".modal-add-user";
-const MODAL_EDIT_USER = ".modal-edit-user";
-const MODAL_CLOSE_ADD_DEVICE = ".modal-close-add-deviсe";
-const MODAL_CLOSE_EDIT_DEVICE = ".modal-close-edit-deviсe";
-const MODAL_CLOSE_ADD_USER = ".modal-close-add-user";
-const MODAL_CLOSE_EDIT_USER = ".modal-close-edit-user";
-const CONTAINER_DEVICE_ADD = ".container-device-add";
-const CONTAINER_DEVICE_EDIT = ".container-device-edit";
-const CONTAINER_USER_ADD = ".container-user-add";
-const CONTAINER_USER_EDIT = ".container-user-edit";
+// Variables with classes for buttons
+const OPEN_MODAL_USER = ".open-modal-user";
+const OPEN_MODAL_DEVICE = ".open-modal-device";
+const CLOSE_MODAL = ".close-modal";
+//Other variables
+const MODAL_ANIMATION_OPEN = "modal-animation-open";
+const MODAL_ANIMATION_CLOSE = "modal-animation-close";
 const MODAL = ".modal";
+const MODAL_DEVICE = "#modal_device";
+const MODAL_USER = "#modal_user";
 
 $(document).ready(function () {
+    // Set the delay time for the animation to work
     let setTime = 500;
 
+    // Animation when opening a modal window
     function openModalAnimation () {
-        $(MODAL).removeClass("modal-animation-close");
-        $(MODAL).addClass("modal-animation-open");
+        $(MODAL).removeClass(MODAL_ANIMATION_CLOSE);
+        $(MODAL).addClass(MODAL_ANIMATION_OPEN);
     }
-
+    // Animation when closing a modal window
     function closeModalAnimation () {
-        $(MODAL).removeClass("modal-animation-open");
-        $(MODAL).addClass("modal-animation-close");
+        $(MODAL).removeClass(MODAL_ANIMATION_OPEN);
+        $(MODAL).addClass(MODAL_ANIMATION_CLOSE);
     }
 
-    /*
-     * Script for modal window new/edit device
-     */
-    // Open modal window (add new device)
-    $(MODAL_ADD_DEVICE).click(function () {
+    // Opening modal windows to add a new / edit device
+    $(OPEN_MODAL_DEVICE).click(function () {
+        if ($(this).data("modal") === "add") {
+            $("#modal_header_device").text("Add new device");
+            $("#modal_device_id").attr("placeholder","Enter device ID");
+            $("#modal_device_key").attr("placeholder","Enter device key");
+            $("#modal_device_type").val("1");
+        } else {
+            $("#modal_header_device").text("Edit device");
+            $("#modal_device_id").attr("placeholder","Edit device ID");
+            $("#modal_device_key").attr("placeholder","Edit device key");
+        }
         openModalAnimation();
-        $(CONTAINER_DEVICE_ADD).css(DISPLAY_FLEX);
+        $(MODAL_DEVICE).css(DISPLAY_FLEX);
     });
-    // Open modal window (edit new device)
-    $(MODAL_EDIT_DEVICE).click(function () {
-       openModalAnimation();
-        $(CONTAINER_DEVICE_EDIT).css(DISPLAY_FLEX);
-    });
-    // Close modal window (add new device)
-    $(MODAL_CLOSE_ADD_DEVICE).click(function () {
-        closeModalAnimation();
-        setTimeout(function () {
-            $(CONTAINER_DEVICE_ADD).css(DISPLAY_NONE);
-            $("#device_id").val('');
-            $("#device_key").val('');
-            $("#device_type").val('1');
-        },setTime);
-    });
-    // Close modal window (edit device)
-    $(MODAL_CLOSE_EDIT_DEVICE).click(function () {
-        closeModalAnimation();
-        setTimeout(function () {
-            $(CONTAINER_DEVICE_EDIT).css(DISPLAY_NONE);
-        },setTime);
-    });
-
-    /*
-     * Script for modal window new/edit user
-     */
-    // Open modal window (add new user)
-    $(MODAL_ADD_USER).click(function () {
+    // Opening modal windows to add a new / edit user
+    $(OPEN_MODAL_USER).click(function () {
+        if ($(this).data("modal") === "add") {
+            $("#modal_header_user").text("Add new user");
+            $("#modal_user_first_name").attr("placeholder","Enter First Name");
+            $("#modal_user_last_name").attr("placeholder","Enter Last Name");
+            $("#modal_user_email").attr("placeholder","Enter email");
+            $("#modal_user_password").attr("placeholder","Enter password");
+            $("#modal_user_confirm_password").attr("placeholder","Confirm password");
+        } else {
+            $("#modal_header_user").text("Edit user");
+            $("#modal_user_first_name").attr("placeholder","Edit First Name");
+            $("#modal_user_last_name").attr("placeholder","Edit Last Name");
+            $("#modal_user_email").attr("placeholder","Edit email");
+            $("#modal_user_password").attr("placeholder","Edit password");
+            $("#modal_user_confirm_password").attr("placeholder","Confirm password");
+        }
         openModalAnimation();
-        $(CONTAINER_USER_ADD).css(DISPLAY_FLEX);
-    });
-    // Open modal window (edit user)
-    $(MODAL_EDIT_USER).click(function () {
-       openModalAnimation();
-        $(CONTAINER_USER_EDIT).css(DISPLAY_FLEX);
-    });
-    // Close modal window (add new user)
-    $(MODAL_CLOSE_ADD_USER).click(function () {
-        closeModalAnimation();
-        setTimeout(function () {
-            $(CONTAINER_USER_ADD).css(DISPLAY_NONE);
-        },setTime);
-    });
-    // Close modal window (edit user)
-    $(MODAL_CLOSE_EDIT_USER).click(function () {
-        closeModalAnimation();
-        setTimeout(function () {
-            $(CONTAINER_USER_EDIT).css(DISPLAY_NONE);
-        },setTime);
+        $(MODAL_USER).css(DISPLAY_FLEX);
     });
 
-
+    // Close all modal windows. With zeroing output
+    $(CLOSE_MODAL).click(function () {
+       if ($(this).data("modal") === "device") {
+           closeModalAnimation();
+           setTimeout(function () {
+               $(MODAL_DEVICE).css(DISPLAY_NONE);
+               $("#modal_device_id").val("");
+               $("#modal_device_key").val("");
+               $("#modal_device_type").val("1");
+           }, setTime);
+       } else {
+           closeModalAnimation();
+           setTimeout(function () {
+               $(MODAL_USER).css(DISPLAY_NONE);
+               $("#modal_user_first_name").val("");
+               $("#modal_user_last_name").val("");
+               $("#modal_user_email").val("");
+               $("#modal_user_password").val("");
+               $("#modal_user_confirm_password").val("");
+           }, setTime);
+       }
+    });
 });
