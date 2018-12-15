7f102df79277f1c089a219b866095059001f6a50
Sorokin-Oleg <sorokin.world@gmail.com>
2018-12-15 13:09:26 +0300

    * Creating logic for the page * Bug fixed

--- static/js/index/modal_box_index_page.js
+++ static/js/index/modal_box_index_page.js
@@ -3,6 +3,7 @@
 const OPEN_MODAL_USER = ".open-modal-user";
 const OPEN_MODAL_DEVICE = ".open-modal-device";
 const CLOSE_MODAL = ".close-modal";
+const TABLE_ROW = ".table-row";
 //Other variables
 const MODAL_ANIMATION_OPEN = "modal-animation-open";
 const MODAL_ANIMATION_CLOSE = "modal-animation-close";
@@ -11,8 +12,9 @@ const MODAL_DEVICE = "#modal_device";
 const MODAL_USER = "#modal_user";
 
 $(document).ready(function () {
-    // Set the delay time for the animation to work
-    let setTime = 500;
+
+    let setTime = 500; // Set the delay time for the animation to work
+    let propsRow = 0; // A helper variable to track the status of a row
 
     // Animation when opening a modal window
     function openModalAnimation () {
@@ -32,14 +34,18 @@ $(document).ready(function () {
             $("#modal_device_id").attr("placeholder","Enter device ID");
             $("#modal_device_key").attr("placeholder","Enter device key");
             $("#modal_device_type").val("1");
-        } else {
+            openModalAnimation();
+            $(MODAL_DEVICE).css(DISPLAY_FLEX);
+        }
+        if ($(this).data("modal") === "edit" && propsRow === 1) {
             $("#modal_header_device").text("Edit device");
             $("#modal_device_id").attr("placeholder","Edit device ID");
             $("#modal_device_key").attr("placeholder","Edit device key");
+            openModalAnimation();
+            $(MODAL_DEVICE).css(DISPLAY_FLEX);
         }
-        openModalAnimation();
-        $(MODAL_DEVICE).css(DISPLAY_FLEX);
     });
+
     // Opening modal windows to add a new / edit user
     $(OPEN_MODAL_USER).click(function () {
         if ($(this).data("modal") === "add") {
@@ -49,16 +55,19 @@ $(document).ready(function () {
             $("#modal_user_email").attr("placeholder","Enter email");
             $("#modal_user_password").attr("placeholder","Enter password");
             $("#modal_user_confirm_password").attr("placeholder","Confirm password");
-        } else {
+            openModalAnimation();
+            $(MODAL_USER).css(DISPLAY_FLEX);
+        }
+        if ($(this).data("modal") === "edit" && propsRow === 1) {
             $("#modal_header_user").text("Edit user");
             $("#modal_user_first_name").attr("placeholder","Edit First Name");
             $("#modal_user_last_name").attr("placeholder","Edit Last Name");
             $("#modal_user_email").attr("placeholder","Edit email");
             $("#modal_user_password").attr("placeholder","Edit password");
             $("#modal_user_confirm_password").attr("placeholder","Confirm password");
+            openModalAnimation();
+            $(MODAL_USER).css(DISPLAY_FLEX);
         }
-        openModalAnimation();
-        $(MODAL_USER).css(DISPLAY_FLEX);
     });
 
     // Close all modal windows. With zeroing output
@@ -82,5 +91,25 @@ $(document).ready(function () {
                $("#modal_user_confirm_password").val("");
            }, setTime);
        }
+    });
+
+    // Function to select a row
+    $(TABLE_ROW).click(function () {
+        $(TABLE_ROW).removeClass("row-active");
+        $(this).addClass("row-active");
+        $('[data-modal="edit"]').addClass("btn-active");
+        $('[data-modal="debug"]').addClass("btn-active");
+        propsRow = 1;
+    });
+
+    // Function to reset row status
+    $(document).mouseup(function (e) {
+        let element = $(".table-main-container");
+        if (!element.is(e.target) && element.has(e.target).length === 0) {
+            $('[data-modal="debug"]').removeClass("btn-active");
+            $('[data-modal="edit"]').removeClass("btn-active");
+            $(TABLE_ROW).removeClass("row-active");
+            propsRow = 0;
+        }
     });
 });
