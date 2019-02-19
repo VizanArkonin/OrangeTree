81fdc9d15ce1b236e368c18a9ca1ea1c8f6c0fda
Sorokin-Oleg <sorokin.world@gmail.com>
2019-02-19 16:38:56 +0300

    * Add value validation for "Last online" timestamp and "Last address" address fields for Devices table; * Add value validation for "Last login" timestamp field for Users table.

--- server/static/js/index/home_page_processing.js
+++ server/static/js/index/home_page_processing.js
@@ -18,6 +18,7 @@ function drawDeviceRow (rowData) {
     let $row = $('<tr class="table-row table-remove-row-device">');
     let onlineStatus = rowData.online;
     let deviceTypeNumber = rowData.device_type;
+    let lastAddress = rowData.last_address;
     const deviceType = {
         1: "Orange Pi Lite",
         2: "Orange Pi Zero",
@@ -30,11 +31,16 @@ function drawDeviceRow (rowData) {
     onlineStatus = onlineStatus === true ? "Online" : "Offline";
 
     $(table.device.content).append($row);
+
     $row.append($("<input data-device-id type=\"hidden\">").val(rowData.id));
     $row.append($("<td data-device-type-id>" + rowData.device_id + "</td>"));
     $row.append($("<td data-device-type>" + deviceType[deviceTypeNumber] + "</td>"));
     $row.children('[data-device-type]').attr('data-device-type', rowData.device_type);
-    $row.append($("<td data-device-last-address>" + rowData.last_address + "</td>"));
+    if ( lastAddress === null) {
+        $row.append($("<td data-device-last-address>" + "N/A" + "</td>"));
+    } else {
+        $row.append($("<td data-device-last-address>" + lastAddress + "</td>"));
+    }
     $row.append($("<td data-device-last-online>" + rowData.last_connected_at + "</td>"));
     if (onlineStatus === "Online") {
         $row.append($("<td data-device-status class='online'>" + onlineStatus + "</td>"));
@@ -60,6 +66,7 @@ function drawUsersTable (data) {
 function drawUserRow(rowData) {
     let $row = $('<tr class="table-row table-remove-row-user">');
     let userEnabled = rowData.active;
+    let lastLogin = rowData.last_login_at;
 
     userEnabled = userEnabled === true ? "Yes" : "No";
 
@@ -70,14 +77,16 @@ function drawUserRow(rowData) {
     $row.children('[data-user-name]').attr('data-first-name', rowData.first_name);
     $row.children('[data-user-name]').attr('data-last-name', rowData.last_name);
     $row.append($("<td data-user-email>" + rowData.email + "</td>"));
-    $row.append($("<td data-user-last-login>" + rowData.last_login_at + "</td>"));
-    
+    if (lastLogin === null) {
+        $row.append($("<td data-user-last-login>" + "N/A" + "</td>"));
+    } else {
+        $row.append($("<td data-user-last-login>" + lastLogin + "</td>")); 
+    }
     if (userEnabled === "Yes") {
         $row.append($("<td data-user-enabled class='online'>" + userEnabled + "</td>"));
     } else {
         $row.append($("<td data-user-enabled class='offline'>" + userEnabled + "</td>"));
     }
-
     $row.children('[data-user-enabled]').attr('data-user-enabled', rowData.active);
 
     $row.append($("<div class='user-roles-container'></div>"));
